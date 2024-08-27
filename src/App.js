04a4236ef8e106cff8e21e4fe05abe3bef07f501
wanget-atsushi <122537062+wanget-atsushi@users.noreply.github.com>
2024-08-27 05:39:01 +0000

    Update Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from 'LandingPage';
-import AnotherComponent from './components/AnotherComponent'; // 他のコンポーネントも適宜インポート
+import LandingPage from './components/LandingPage';
+//import AnotherComponent from './components/AnotherComponent'; // 他のコンポーネントも適宜インポート
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/another" element={<AnotherComponent />} />
+//        <Route path="/another" element={<AnotherComponent />} />
         {/* 他のルートを追加 */}
       </Routes>
     </Router>
