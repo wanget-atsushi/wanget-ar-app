3403ee13de86bc331119811cdec53f420614a3ee
wanget-atsushi <122537062+wanget-atsushi@users.noreply.github.com>
2024-08-27 05:45:43 +0000

    Update Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-//import AnotherComponent from './components/AnotherComponent'; // 他のコンポーネントも適宜インポート
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-//        <Route path="/another" element={<AnotherComponent />} />
-        {/* 他のルートを追加 */}
       </Routes>
     </Router>
   );
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import QRCode from 'qrcode.react';
 import { useLocation } from 'react-router-dom';
-import { supabase } from './supabaseClient';
+import { supabase } from '../supabaseClient';
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
